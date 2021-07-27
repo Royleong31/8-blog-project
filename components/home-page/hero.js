8589e52a3ce5689344bbd7b41fcc4c4190a7cdac
Roy Leong <l3ongroy@gmail.com>
2021-07-27 14:18:49 +0800

    added priority true to hero image

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -5,7 +5,13 @@ export default function Hero() {
 	return (
 		<section className={classes.hero}>
 			<div className={classes.image}>
-				<Image src="/images/site/anna.jpg" alt="An image of Anna" width={"300"} height={"300"} />
+				<Image
+					priority={true}
+					src="/images/site/anna.jpg"
+					alt="An image of Anna"
+					width={"300"}
+					height={"300"}
+				/>
 			</div>
 			<h1>Hi, Im Anna</h1>
 			<p>I blog about web development, especially about Angular and React</p>
